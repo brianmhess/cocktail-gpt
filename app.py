de7401e2297cd0b24f818f803ddb6ffb2e3d6525
Brian Hess <brianmhess@users.noreply.github.com>
2023-01-16 18:10:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import streamlit as st
 import openai
 import json
 
-st.markdown("## CocktailGPT")
+with st.expander("About CocktailGPT"):
+    st.markdown("""
+CocktailGPT is an app I built that allows you
+to ask ChatGPT for a cocktail recipe. 
+""")
 
 openai.api_key = st.secrets["OPENAI_KEY"]
 
-def get_via_openai_named(name):
+def get_via_openai(input):
     data = {
                 "model": "text-davinci-003",
-                "prompt": 'Show me a recipe in ounces for a cocktail named ' + name + '. Your answer should be in valid JSON format for Python. For example: {"name": "My Cocktail", "description": "This is a nice cocktail for a cold day", "ingredients": [ {"ingredient": "Bourbon whiskey", "amount": "2 ounces"}, {"ingredient": "Sweet vermouth", "amount": "1 ounce"}, {"ingredient": "Angostura bitters", "amount": "2 dashes"}], "instructions": "Combine all ingredients in a mixing glass with ice. Stir until chilled. Strain into glass", "glass": "Coupe glass", "garnish": "Maraschino cherry", "suggestions": "You can also use Rye instead of Bourbon." }',
+                "prompt": input + '. Your answer should be in ounces. Your answer should be in valid JSON format for Python. For example: {"name": "My Cocktail", "description": "This is a nice cocktail for a cold day", "ingredients": [ {"ingredient": "Bourbon whiskey", "amount": "2 ounces"}, {"ingredient": "Sweet vermouth", "amount": "1 ounce"}, {"ingredient": "Angostura bitters", "amount": "2 dashes"}], "instructions": "Combine all ingredients in a mixing glass with ice. Stir until chilled. Strain into glass", "glass": "Coupe glass", "garnish": "Maraschino cherry", "suggestions": "You can also use Rye instead of Bourbon." }',
                 "max_tokens": 256,
                 "temperature": 0.10,
                 "n": 1
@@ -17,8 +21,7 @@ def get_via_openai_named(name):
     ai_resp = openai.Completion.create(**data)
     return ai_resp
 
-def show_recipe(rstr):
-    rj = json.loads(rstr)
+def show_recipe(rj):
     st.markdown(f'## :blue[{rj["name"]}]')
     st.markdown(f':blue[**Description**]: {rj["description"]}')
     st.markdown(f'### Ingredients')
@@ -30,11 +33,140 @@ def show_recipe(rstr):
     st.markdown(f':blue[**Garnish**]: {rj["garnish"]}')
     st.markdown(f':blue[**Suggestions**]: {rj["suggestions"]}')
     
+ingredients = ['Sugar syrup',
+'Lime juice',
+'Lemon juice',
+'London dry gin',
+'Vodka',
+'Light white rum',
+'Angostura Aromatic Bitters',
+'Orange juice',
+'Triple sec liqueur',
+'Dry vermouth',
+'Cognac V',
+'Pineapple juice',
+'Sweet vermouth',
+'Bourbon whiskey',
+'Apple juice',
+'Pomegranate (grenadine) syrup',
+'Tequila (reposado)',
+'Egg white',
+'Orange bitters',
+'Cranberry juice (red)',
+'Grand Marnier liqueur',
+'Soda (club soda)',
+'Cream',
+'Maraschino liqueur',
+'Blended Scotch whisky',
+'Fresh mint leaves/sprigs',
+'Brut champagne',
+'Grapefruit juice (pink)',
+'Elderflower liqueur',
+'Absinthe',
+'Apricot brandy liqueur',
+'Coffee liqueur',
+'Campari Bitter',
+'Calvados apple brandy',
+'Amaretto liqueur',
+'Milk',
+'Black raspberry liqueur',
+'Runny honey',
+'Bénédictine D',
+'White crème de cacao liqueur',
+'Peychaud's aromatic bitters',
+'Aged rum (+7 year old)',
+'Citrus flavoured vodka',
+'Chartreuse Vert (green)',
+'Cherry brandy liqueur',
+'Irish cream liqueur',
+'Almond (orgeat) syrup',
+'Cachaça',
+'Lime cordial',
+'Raspberries (fresh)',
+'Golden rum',
+'Ginger ale',
+'Crème de cassis',
+'Melon liqueur (green)',
+'Galliano L'Autentico liqueur',
+'Blue curaçao liqueur',
+'Drambuie liqueur',
+'Vanilla infused vodka',
+'Straight rye whiskey',
+'Hazelnut liqueur',
+'Żubrówka bison grass vodka',
+'Fino sherry',
+'Maple syrup',
+'Islay single malt Scotch whisky',
+'Agave syrup',
+'Chartreuse Jaune (yellow)',
+'Peach Schnapps liqueur',
+'Crème de banane liqueur',
+'Port wine',
+'White wine (Sauvignon Blanc)',
+'Pisco',
+'Apple schnapps liqueur',
+'Ginger beer',
+'Falernum liqueur',
+'Lemonade/Sprite/7-Up',
+'Coconut rum liqueur',
+'White crème de menthe',
+'Ginger liqueur',
+'Southern Comfort liqueur',
+'Dark crème de cacao liqueur',
+'Basil leaves',
+'Dubonnet Red',
+'Passion fruit syrup',
+'Jenever',
+'Vodka raspberry flavoured',
+'Sake',
+'Overproof rum (white)',
+'Strawberries (fresh)',
+'Ginger (fresh root)',
+'Navy rum',
+'Prosecco sparkling wine',
+'Vanilla sugar syrup',
+'Passion fruit (fresh)',
+'Lillet Blanc',
+'Honey sugar syrup',
+'Espresso coffee (freshly made)',
+'Black pepper',
+'Tonic water',
+'Anise liqueur',
+'Crème de framboise liqueur'
+]
 
-name = st.text_input("Show me a cocktail named")
+prompts = ["Show me a cocktail named", "Show me a cocktail including/excluding", "Show me a cocktail (freeform)"] 
+prompt = st.select("What sort of cocktail", options=prompts, index=0)
 
-if (len(name) > 0):
-    rec = get_via_openai_named(name)
-    for ch in rec["choices"]:
-        show_recipe(ch["text"])
+input = ""
+if (prompt == "Show me a cocktail named"):
+    name = st.text_input("Show me a cocktail named")
+    if (len(name) > 0):
+        input = f'Show me a cocktail named {name}'
+
+if (prompt == "Show me a cocktail including/excluding"):
+    including = st.multiselect("Include these ingredients", options=ingredients)
+    excluding = st.multiselect("Exclude these ingredients", options=ingredients)
+    if (len(including)>0):
+        input = f"Show me a cocktail including {','.join(including)} and other ingredients"
+        if (len(excluding)>0):
+            input = input + f" but does not include {','.join(excluding)}"
+    else:
+        if (len(excluding)>0):
+            input = f"Show me a cocktail which does not include {','.join(excluding)}"
+
+if (prompt = "Show me a cocktail (freeform)"):
+    freetext = st.text_input("Fill in the blank: Shoe me a cocktail ____")
+    if (len(freetext)>0):
+        input = f"Show me a cocktail {freetext}"
+
+if (len(input) > 0):
+    rec = get_via_openai(input)
     st.sidebar.json(rec["choices"])
+    recch = rec["choices"][0]
+    choices = json.loads(recch["text"])
+    if (type(choices) != "list"):
+        choices = [choices]
+    for ch in choices:
+        show_recipe(ch["text"])
+
